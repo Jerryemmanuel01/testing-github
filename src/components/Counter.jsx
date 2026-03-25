e72050e3fc53216edc9446eac1a1c110cea2889f
jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2026-03-25 12:32:19 +0100

    feat: useEffect added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Counter = () => {
+    const [refetch, setRefetch] = useState("");
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   const [numHold, setNumHold] = useState(0);
   const [textTyped, setTextTyped] = useState("");
 
@@ -13,14 +19,19 @@ const Counter = () => {
 
   return (
     <div className="px-6 my-10">
-      <form className="my-4" onSubmit={(e)=> e.preventDefault()}>
+      <form className="my-4" onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
           className="border  border-gray-600 rounded-md outline-none px-2 py-2"
           onChange={(e) => setTextTyped(e.target.value)}
           value={textTyped}
         />
-      <button onClick={()=>console.log(textTyped)} className="border border-gray-500 py-1 px-4 rounded-md bg-slate-200 ">submit</button>
+        <button
+          onClick={() => console.log(textTyped)}
+          className="border border-gray-500 py-1 px-4 rounded-md bg-slate-200 "
+        >
+          submit
+        </button>
       </form>
       <p className="font-bold text-4xl">{numHold}</p>
       <div className="flex gap-4 mt-3">
@@ -51,16 +62,12 @@ const Counter = () => {
           rendering this using the && operator
         </span>
       )}
+
+      <button className="" onClick={() => setRefetch((prev) => !prev)}>
+        Refetch
+      </button>
     </div>
   );
 };
 
 export default Counter;
-
-echo "# testing-github" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Jerryemmanuel01/testing-github.git
-git push -u origin main
